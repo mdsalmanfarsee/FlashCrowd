ea1e20a0b09f90cd077d8709f1b57db55535d76c
mdsalmanfarsee <far90986@gmail.com>
2025-06-20 20:56:57 +0530

    add files in gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,25 +5,31 @@ import {
   CivicAuthProvider,
   UserButton
 } from "@civic/auth/react";
+import Map from "./components/Map";
+
+
 
 export default function App({ children }) {
-  const iframeContainerRef = useRef(null);
+  //const iframeContainerRef = useRef(null);
 
   return (
-    <>
-      {/* Required container for embedded iframe */}
-      <div ref={iframeContainerRef} id="civic-iframe-container" />
+    // <>
+    //   {/* Required container for embedded iframe */}
+    //   <div ref={iframeContainerRef} id="civic-iframe-container" />
 
-      <CivicAuthProvider
-        clientId="437291ef-bb31-488c-a0ec-5bd511ff8cf6"
-        displayMode="iframe"
-        iframeDisplayMode="modal"
-        targetContainerElement={iframeContainerRef}
-      >
-        <UserButton />
-        {children}
-      </CivicAuthProvider>
-    </>
+    //   <CivicAuthProvider
+    //     clientId="437291ef-bb31-488c-a0ec-5bd511ff8cf6"
+    //     displayMode="iframe"
+    //     iframeDisplayMode="modal"
+    //     targetContainerElement={iframeContainerRef}
+    //   >
+    //     <UserButton />
+    //     {children}
+    //   </CivicAuthProvider>
+    // </>
+
+    <Map />
+
   );
 }
 
